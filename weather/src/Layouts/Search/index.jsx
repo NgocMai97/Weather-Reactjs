e15e5ffa1717d463d9a7a01d88ef28e0862788e2
MaiNN1 <mainn@arrowhitech.net>
2022-06-05 22:46:46 +0700

    Mainn search api

--- weather/src/Layouts/Search/index.jsx
+++ weather/src/Layouts/Search/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import cityApi from "../../api/cityApi";
 import weatherApi from "../../api/weatherApi";
 function SearchForm( props) {

3fdd0523faaf6ebe9fe8b10e9bddcc51715614ac
hoangvanngoc <hoangngocbkhn2311@gmail.com>
2022-06-07 13:28:49 +0700

    Hour config

--- weather/src/pages/Hour/index.js
+++ weather/src/pages/Hour/index.js
@@ -1,10 +1,22 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Chart from 'chart.js/auto'
 import weatherApi from '~/api/weatherApi'
-
+import styles from './Hour.module.scss'
+import classNames from "classnames/bind";
 
 function Hour() {
     let chartHour = null;
+    const cx = classNames.bind(styles)
+
+    var hourArray = [],
+        d = new Date(),
+        h = d.getHours(),
+
+        ampm = h >= 12 ? 'PM' : 'AM';
+
+        for (var i = h; i < 24; i++) {
+            hourArray.push((i%12 + ':' + '00') + ' ' +  ampm);
+        }
 
     useEffect(() => {
         const fetProduct = async () => {
@@ -20,19 +32,19 @@ function Hour() {
         const config = {
             type: 'line',
             data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange','Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange','Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                labels: hourArray,
                 datasets: [{
                     label: 'Temp (°C )',
                     data: [12, 19, 3, 5, 2, 3,12, 19, 3, 5, 2, 3,12, 19, 3, 5, 2, 3,12, 19, 3, 5, 2, 3],
                     backgroundColor: 'transparent',
-                    borderColor: 'red',
+                    borderColor: '#3cba9f',
                     borderWidth: 4
                 },
                 {
                     label: 'Feel like (°C )',
                     data: [6, 14, 5, 5, 3, 2,6, 4, 5, 5, 3, 2,6, 14, 5, 5, 3, 2,6, 14, 5, 5, 3, 2],
                     backgroundColor: 'transparent',
-                    borderColor: 'green',
+                    borderColor: '#8e5ea2',
                     borderWidth: 4
                 }
             ]
@@ -43,9 +55,22 @@ function Hour() {
                         tension: 0
                     }
                 },
+                animations: {
+                    tension: {
+                        duration: 1000,
+                        easing: 'linear',
+                        from: 1,
+                        to: 0,
+                        loop: true
+                    }
+                }
+                ,
                 scales: {
                     y: {
-                        beginAtZero: true
+                        beginAtZero: true,
+                          steps: 2,
+                          stepValue: 6,
+                          max: 60 //max value for the chart is 60
                     }
                 }
             }
@@ -59,7 +84,7 @@ function Hour() {
 }, [])
 
     return (
-        <div className="chart-wrap">
+        <div className={cx('chart-wrap', 'bg-white')}>
             <canvas id="Canvas" width="900" height="600"></canvas>
         </div>
     )
